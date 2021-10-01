a99ea60a9eeb21890351af9d0ed402eed28b81d1
Kevin Läufer <laeufer@cs.berkeley.edu>
2021-10-01 12:52:38 -0700

    run test with verilator and generate FST

--- src/test/scala/freechips/rocketchip/tilelink/XbarTests.scala
+++ src/test/scala/freechips/rocketchip/tilelink/XbarTests.scala
@@ -8,14 +8,14 @@ import freechips.rocketchip.diplomacy._
 import freechips.rocketchip.subsystem.WithoutTLMonitors
 class XbarTests extends AnyFreeSpec with ChiselScalatestTester {
   "simple xbar test" in {
-    //implicit val p: Parameters = new WithoutTLMonitors
-    implicit val p: Parameters = Parameters.empty
-    test(new TLMulticlientXbarTest(nManagers = 2, nClients = 2)).withAnnotations(Seq(WriteVcdAnnotation)) { dut =>
-      dut.clock.setTimeout(-1)
+    implicit val p: Parameters = new WithoutTLMonitors
+    //implicit val p: Parameters = Parameters.empty
+    test(new TLMulticlientXbarTest(nManagers = 2, nClients = 2)).withAnnotations(Seq(WriteFstAnnotation, VerilatorBackendAnnotation)) { dut =>
+      dut.clock.setTimeout(0)
       dut.io.start.poke(true.B)
       dut.clock.step()
       dut.io.start.poke(false.B)
-      dut.clock.step(5000)
+      dut.clock.step(150000)
     }
   }
 }
